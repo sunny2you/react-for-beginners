faa823413447db93aa8f3bfeba4f36f4fac5bdfe
sunny2you <ally1008@naver.com>
2024-03-22 15:05:14 +0900

    todolists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {useState} from "react";
+
+function App(){
+
+  const[toDo,setToDo]=useState("");
+  const[toDos,setToDos]=useState([]);
+  const onChange=(event)=>setToDo(event.target.value);
+  const onSubmit=(event)=>{
+    event.preventDefault();
+    if(toDo===""){
+      return;
+    } 
+    setToDos((currentArray)=>[toDo, ...currentArray])
+    setToDo("");
+  }
+  console.log(toDos);
+  return (<div>
+    <h1>My To Dos {toDos.length}</h1>
+    <form onSubmit={onSubmit}>
+    <input 
+      value={toDo} 
+      onChange={onChange} 
+      type="text" 
+      placeholder="Write your to do..."
+    />
+    <button>Add To Do</button>
+    </form>
+    <hr/>
+    <ul>
+    {toDos.map((item,index)=> <li key={index}>{item}</li>)}
+    </ul>
+   </div>)
+
 }
 
 export default App;
